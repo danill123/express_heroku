800992fb787ac2a98765f29e9b2b044eab016dae
Danill Yudhistira Setiawan <danillyudhistira@gmail.com>
2020-11-08 13:02:12 +0700

    update auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.auth = async(req, res) => {
             let dat = await User.findOne({email: req.body.email})
             let verify = await bcrypt.compare(req.body.password, dat["password"]);
             if(verify) {
-                res.status(200).send({message: 'Authentication Success', auth: true, status: 200})
+                res.status(200).send({message: 'Authentication Success', auth: true, data_user: dat ,status: 200})
             } else {
                 res.status(200).send({message: 'Authentication failed', auth: false, status: 200})
             }
