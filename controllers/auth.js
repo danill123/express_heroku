4746786b22143b9b0d3397082e01a1be9aeb9a7a
Danill Yudhistira Setiawan <danillyudhistira@gmail.com>
2020-11-12 09:16:44 +0700

    fixing authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,14 +7,18 @@ exports.auth = async(req, res) => {
             res.status(400).send({message: 'Parameter not complete'})
         } else {
             let dat = await User.findOne({email: req.body.email})
-            let verify = await bcrypt.compare(req.body.password, dat["password"]);
-            if(verify) {
-                res.status(200).send({message: 'Authentication Success', auth: true, data_user: dat ,status: 200})
+            if(dat) {
+                let verify = await bcrypt.compare(req.body.password, dat["password"]);
+                if(verify) {
+                    res.status(200).send({message: 'Authentication Success', auth: true, data_user: dat ,status: 200})
+                } else {
+                    res.status(200).send({message: 'Authentication failed', auth: false, status: 200})
+                }
             } else {
                 res.status(200).send({message: 'Authentication failed', auth: false, status: 200})
             }
         }
     } catch (error) {
-        res.status(500).send({message: 'internal'})
+        res.status(500).send({message: 'Internal Error'})
     }
 }
